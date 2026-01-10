fe2b629ebd8639ed050029a953ebb718555a543d
mlopez8763 <miguel.l.business@gmail.com>
2026-01-09 23:44:59 -0700

    Simulation for computing sampled std. dev.

---
+++ Chapter_04_Noise_Analysis/Ch04_L03_Sampled_StdDev.m
@@ -0,0 +1,40 @@
+clc; close all; clear all; format long; format compact;
+
+% MATLAB script computes the sampled standard deviation without using any
+% of the MATLAB's internal math functions. 
+
+% Preallocate variables 
+Z = zeros(1, 100);
+count = 0; % iteration counter variable
+Z1 = 0;
+
+for i = 1:100
+    SUM = 0;
+    for j = 1:12
+        RAND = rand(1);
+        SUM = SUM + RAND;
+    end
+    X = SUM - 6;
+    Z(i) = X;
+    Z1 = Z(i) + Z1;
+    XMEAN = Z1/i;
+end
+SIGMA = 0;
+Z1 = 0;
+for i = 1:100
+    Z1 = (Z(i)-XMEAN)^2 + Z1;
+    if i == 1
+        SIGMA = 0;
+    else
+        SIGMA = sqrt(Z1/(i-1));
+    end
+    count = count + 1;
+    array_i(count) = i;
+    array_SIGMA(count) = SIGMA;
+
+end
+figure(1);
+plot(array_i, array_SIGMA); grid on;
+title('Sampled Standard Deviation');
+xlabel('Number of Samples');
+ylabel('Calculated Standard Deviation');
