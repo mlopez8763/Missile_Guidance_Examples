cbfd96ac7c9fc6a14bf9b531d9fe94cb4eb61319
mlopez8763 <miguel.l.business@gmail.com>
2026-01-09 23:59:23 -0700

    Minor documentation

--- Chapter_04_Noise_Analysis/Ch04_L03_Sampled_StdDev.m
+++ Chapter_04_Noise_Analysis/Ch04_L03_Sampled_StdDev.m
@@ -1,32 +1,34 @@
 clc; close all; clear all; format long; format compact;
 
 % MATLAB script computes the sampled standard deviation without using any
-% of the MATLAB's internal math functions. 
+% of the MATLAB's internal math functions. More specifically, this is a
+% Monte Carlo simulation to estimate standard  deviation.
 
 % Preallocate variables 
 Z = zeros(1, 100);
-count = 0; % iteration counter variable
-Z1 = 0;
+count = 0;  % iteration counter variable
+Z1 = 0;     % mean of sampel set "Z"
 
+% Generate Gaussian-distributed samples
 for i = 1:100
     SUM = 0;
     for j = 1:12
         RAND = rand(1);
         SUM = SUM + RAND;
     end
-    X = SUM - 6;
+    X = SUM - 6;    % zero-mean Gaussian adjustmenet
     Z(i) = X;
     Z1 = Z(i) + Z1;
     XMEAN = Z1/i;
 end
 SIGMA = 0;
-Z1 = 0;
+Z1 = 0;     % now square deviations
 for i = 1:100
     Z1 = (Z(i)-XMEAN)^2 + Z1;
     if i == 1
         SIGMA = 0;
     else
-        SIGMA = sqrt(Z1/(i-1));
+        SIGMA = sqrt(Z1/(i-1)); % sample standard dev.
     end
     count = count + 1;
     array_i(count) = i;
